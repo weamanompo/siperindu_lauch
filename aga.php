413bb54be2670fcf722fcfd6545343370cb09db4
weamanompo <weamanompo@gmail.com>
2023-10-18 05:43:29 +0700

    tes input

--- aga.php
+++ aga.php
@@ -4,43 +4,41 @@ include 'koneksi.php';
 
 $aga = mysqli_query($koneksi, "SELECT * FROM api ORDER BY id desc");
 
-if (isset($_POST["login"])) {
-
-    $ag = $_POST['aga'];
-
-    $query2 = "INSERT INTO api
-
-VALUES 
-
-('',  '$ag')";
-
-    mysqli_query($koneksi, $query2);
-}
-
-
 ?>
-
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aga</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-
+<style>
+#content {
+  max-width: 500px;
+  margin: auto;
+}
+</style>
 <body>
-    <h4>AGA</h4>
-    <form method="post">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label"></label>
-            <input type="text" class="form-control" name="aga">
+    <h4></h4>
+    <form method="post" action="isi.php">
+        <div class="card" style="width: 18rem;" id="content">
+            <div class="card-body">
+                <h5 class="card-title">Form</h5>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label"></label>
+                    <input type="text" class="form-control" name="aga">
+                </div>
+                <button type="submit" name="login" class="btn btn-primary">Submit</button>
+            </div>
         </div>
-        <button type="submit" name="login" class="btn btn-primary">Submit</button>
     </form>
     <hr>
-    <table class="table">
+    <div class="card" style="width: 18rem;" id="content">
+            <div class="card-body">
+                <h5 class="card-title">
+                <div class="mb-3">
+                <table class="table">
         <thead>
             <tr>
                 <th scope="col"></th>
@@ -55,6 +53,7 @@ VALUES
             <?php endwhile; ?>
         </tbody>
     </table>
+        </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
---
+++ isi.php
@@ -0,0 +1,31 @@
+<?php
+
+include 'koneksi.php';
+
+if (isset($_POST["login"])) {
+
+    $ag = $_POST['aga'];
+
+    $query2 = "INSERT INTO api
+
+VALUES 
+
+('',  '$ag')";
+
+    mysqli_query($koneksi, $query2);
+}
+
+if (mysqli_affected_rows($koneksi) > 0) {
+
+
+    echo "<script>alert('Proses Penambahan Data Berhasil');
+document.location.href = 'aga.php';
+</script>";
+}else {
+
+    echo "<script>alert('Proses Penambahan data Gagal');
+document.location.href = 'aga.php';
+</script>";
+}
+
+?>
