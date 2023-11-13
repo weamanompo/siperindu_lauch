984a203fc25f4a57ad2cdddcf1bb1c61032a89e2
weamanompo <weamanompo@gmail.com>
2023-11-13 11:46:38 +0700

    rindu

---
+++ code.php
@@ -0,0 +1,142 @@
+<?php
+
+require 'koneksi.php';
+
+if(isset($_POST['save_student']))
+{
+    $name = mysqli_real_escape_string($koneksi, $_POST['name']);
+
+
+    if($name == NULL )
+    {
+        $res = [
+            'status' => 422,
+            'message' => 'All fields are mandatory'
+        ];
+        echo json_encode($res);
+        return;
+    }
+
+    $query = "INSERT INTO api (api_wa) VALUES ('$name')";
+    $query_run = mysqli_query($koneksi, $query);
+
+    if($query_run)
+    {
+        $res = [
+            'status' => 200,
+            'message' => 'Student Created Successfully'
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 500,
+            'message' => 'Student Not Created'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+
+if(isset($_POST['update_student']))
+{
+    $student_id = mysqli_real_escape_string($koneksi, $_POST['student_id']);
+
+    $name = mysqli_real_escape_string($koneksi, $_POST['name']);
+
+    if($name == NULL)
+    {
+        $res = [
+            'status' => 422,
+            'message' => 'All fields are mandatory'
+        ];
+        echo json_encode($res);
+        return;
+    }
+
+    $query = "UPDATE api SET api_wa ='$name' 
+                WHERE id='$student_id'";
+    $query_run = mysqli_query($koneksi, $query);
+
+    if($query_run)
+    {
+        $res = [
+            'status' => 200,
+            'message' => 'Student Updated Successfully'
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 500,
+            'message' => 'Student Not Updated'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+
+if(isset($_GET['student_id']))
+{
+    $student_id = mysqli_real_escape_string($koneksi, $_GET['student_id']);
+
+    $query = "SELECT * FROM api WHERE id='$student_id'";
+    $query_run = mysqli_query($koneksi, $query);
+
+    if(mysqli_num_rows($query_run) == 1)
+    {
+        $student = mysqli_fetch_array($query_run);
+
+        $res = [
+            'status' => 200,
+            'message' => 'Student Fetch Successfully by id',
+            'data' => $student
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 404,
+            'message' => 'Student Id Not Found'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+if(isset($_POST['delete_student']))
+{
+    $student_id = mysqli_real_escape_string($koneksi, $_POST['student_id']);
+
+    $query = "DELETE FROM api WHERE id='$student_id'";
+    $query_run = mysqli_query($koneksi, $query);
+
+    if($query_run)
+    {
+        $res = [
+            'status' => 200,
+            'message' => 'Student Deleted Successfully'
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 500,
+            'message' => 'Student Not Deleted'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+?>
